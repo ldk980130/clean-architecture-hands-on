238758a58b116658e9f8b3c5d67a157f8f4cbede
does <ldk980130@gmail.com>
2023-06-29 15:48:11 +0900

    chore: 프로젝트 세팅

---
+++ src/main/java/com/practice/cleanarichitecturehandson/CleanArichitectureHandsOnApplication.java
@@ -0,0 +1,13 @@
+package com.practice.cleanarichitecturehandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArichitectureHandsOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArichitectureHandsOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/cleanarichitecturehandson/CleanArichitectureHandsOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.cleanarichitecturehandson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArichitectureHandsOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
