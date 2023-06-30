7a5ae00e300532c4182f621d1ba0d1dba7cb0f03
does <ldk980130@gmail.com>
2023-06-30 17:34:51 +0900

    feat: SendMoneyCommand 검증 추가

--- src/main/java/com/practice/cleanarichitecturehandson/buckpal/account/application/port/in/SendMoneyCommand.java
+++ src/main/java/com/practice/cleanarichitecturehandson/buckpal/account/application/port/in/SendMoneyCommand.java
@@ -1,4 +1,17 @@
 package com.practice.cleanarichitecturehandson.buckpal.account.application.port.in;
 
-public class SendMoneyCommand {
+import com.practice.cleanarichitecturehandson.buckpal.account.domain.AccountId;
+import com.practice.cleanarichitecturehandson.buckpal.account.domain.Money;
+import jakarta.validation.constraints.NotNull;
+
+public record SendMoneyCommand(
+        @NotNull
+        AccountId sourceAccountId,
+
+        @NotNull
+        AccountId targetAccountId,
+
+        @NotNull
+        Money money
+) {
 }
